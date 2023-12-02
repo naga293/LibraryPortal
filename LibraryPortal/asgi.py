d58776203b296c341e51dcaf1630d65d998add6e
naga293 <nagasrihari786@gmail.com>
2023-12-02 17:19:58 +0530

    updated project name

--- LibraryPortal/asgi.py
+++ LibraryPortal/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for StudentProject project.
+ASGI config for LibraryPortal project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'StudentProject.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LibraryPortal.settings')
 
 application = get_asgi_application()
